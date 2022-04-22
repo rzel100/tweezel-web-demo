64e581136726cbe4d11ab623a78f7009e05a76e6
Mohamad Arizal Hadisucipto <62789747+rzel100@users.noreply.github.com>
2022-04-23 00:52:08 +0700

    Delete assets directory

--- assets/AboutView.3f8564f4.js
+++
@@ -1,1 +0,0 @@
-import{o as s,c,a as n}from"./index.176f91dd.js";var r=(e,t)=>{const o=e.__vccOpts||e;for(const[_,a]of t)o[_]=a;return o};const i={},u={class:"about"},l=n("h1",null,"This is an about page",-1),d=[l];function v(e,t){return s(),c("div",u,d)}var p=r(i,[["render",v]]);export{p as default};
